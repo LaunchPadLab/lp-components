ac363ad4fcec7271f936f87d4780095e9efa57c2
David Pickart <davidstoreypickart@gmail.com>
2017-03-17 11:30:49 -0500

    Add input tests

--- test/forms/input.test.js
+++ test/forms/input.test.js
@@ -1,54 +1,62 @@
 import React from 'react'
 import { shallow, render } from 'enzyme'
 import { Input } from '../../src/'
-import { startCase, stripNamespace } from '../../src/utils'
 
 const name = 'name.of.field'
-// const label = 'label'
-// const error = 'FAIL!'
+const formattedName = 'Field'
+const label = 'label'
+const error = 'input error'
 
-const formatLabel = (name) => startCase(stripNamespace(name))
+/* 
+  Note: when using render(), we have to use length instead of exists() 
+  because it's implemented using the Cheerio API.
+  https://github.com/cheeriojs/cheerio/issues/798
+*/
 
 test('when label not provided - renders a label with content equal to formatted input name', () => {
   const props = { input: { name }, meta: {} }
   const wrapper = render(<Input { ...props }/>)
-  expect(wrapper.find('label').text()).toBe(formatLabel(name))
+  expect(wrapper.find('label').text()).toBe(formattedName)
 })
 
-// test('when label is a string - renders a label with content equal to string', t => {
-//   const props = { input: { name }, meta: {}, label }
-//   const wrapper = shallow(<Input { ...props }/>)
-//   t.is(wrapper.find('.label').exists(), true)
-//   t.is(wrapper.find('.label').text(), label)
-// })
+test('when label is a string - renders a label with content equal to string', () => {
+  const props = { input: { name }, meta: {}, label }
+  const wrapper = render(<Input { ...props }/>)
+  expect(wrapper.find('label').text()).toBe(label)
+})
 
-// test('when label is false - does not render a label', t => {
-//   const props = { input: { name }, meta: {}, label: false }
-//   const wrapper = shallow(<Input { ...props }/>)
-//   t.is(wrapper.find('.label').exists(), false)
-// })
+test('when label is false - does not render a label', () => {
+  const props = { input: { name }, meta: {}, label: 'hey!' }
+  const wrapper = render(<Input { ...props }/>)
+  expect(wrapper.find('label').length).toBe(1)
+})
 
-// test('when error not provided - does not render the error message', t => {
-//   const props = { input: { name }, meta: {} }
-//   const wrapper = shallow(<Input { ...props }/>)
-//   t.is(wrapper.find('.error-message').exists(), false)
-// })
+test('when error not provided - does not render the error message', () => {
+  const props = { input: { name }, meta: {} }
+  const wrapper = render(<Input { ...props }/>)
+  expect(wrapper.find('.error-message').length).toBe(0)
+})
 
-// test('when error not provided - does not add error class to container', t => {
-//   const props = { input: { name }, meta: {} }
-//   const wrapper = shallow(<Input { ...props }/>)
-//   t.is(wrapper.find('.error').exists(), false)
-// })
+test('if input is invalid but not touched - does not add error class to container', () => {
+  const props = { input: { name }, meta: { invalid: true, touched: false } }
+  const wrapper = shallow(<Input { ...props }/>)
+  expect(wrapper.find('.error').exists()).toBe(false)
+})
 
-// test('when error is provided - adds the error class to container', t => {
-//   const props = { input: { name }, meta: { error } }
-//   const wrapper = shallow(<Input { ...props }/>)
-//   t.is(wrapper.find('.error').exists(), true)
-// })
+test('if input is touched but not invalid - does not add error class to container', () => {
+  const props = { input: { name }, meta: { invalid: false, touched: true } }
+  const wrapper = shallow(<Input { ...props }/>)
+  expect(wrapper.find('.error').exists()).toBe(false)
+})
 
-// test('when error is provided - renders the error message containing the error', t => {
-//   const props = { input: { name }, meta: { error } }
-//   const wrapper = shallow(<Input { ...props }/>)
-//   t.is(wrapper.find('.error-message').exists(), true)
-//   t.is(wrapper.find('.error-message').text(), error)
-// })
+test('if input is touched and invalid - adds the error class to container', () => {
+  const props = { input: { name }, meta: { invalid: true, touched: true } }
+  const wrapper = shallow(<Input { ...props }/>)
+  expect(wrapper.find('.error').exists()).toBe(true)
+})
+
+test('when error is provided - renders the error message containing the error', () => {
+  const props = { input: { name }, meta: { invalid: true, touched: true, error } }
+  const wrapper = render(<Input { ...props }/>)
+  expect(wrapper.find('.error-message').text()).toBe(error)
+})
