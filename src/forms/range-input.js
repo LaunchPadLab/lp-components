a50b1fd6946926dd67c578e1cb0214b5bdd87f0c
David Pickart <davidstoreypickart@gmail.com>
2017-03-17 15:32:31 -0500

    Add range input

---
+++ src/forms/range-input.js
@@ -0,0 +1,55 @@
+import React, { PropTypes } from 'react'
+import classnames from 'classnames'
+import fieldPropTypes from './field-proptypes'
+import InputError from './input-error'
+import InputLabel from './input-label'
+
+const propTypes = {
+  ...fieldPropTypes,
+  ...InputLabel.propTypes,
+  ...InputError.propTypes,
+  type: PropTypes.string,
+  min: PropTypes.number,
+  max: PropTypes.number,
+  step: PropTypes.number
+}
+
+const defaultProps = {
+  min: 0,
+  max: 100,
+  step: 1
+}
+
+function RangeInput ({
+  input: { name, value, onBlur, onChange },
+  meta: { error, pristine, touched, invalid },
+  className,
+  hint,
+  label,
+  tooltip,
+  min,
+  max,
+  step,
+  ...rest
+}) {
+  return (
+    <fieldset className={ classes({ className, touched, invalid }) }>
+      <InputLabel { ...{ hint, label, name, tooltip } } />
+      <input 
+        type="range"
+        onBlur={ pristine ? null : onBlur } 
+        { ...{ id: name, name, value, onChange, min, max, step, ...rest } }
+      />
+      <InputError { ...{ error, invalid, touched } } />
+    </fieldset>
+  )
+}
+
+function classes ({ className, touched, invalid }) {
+  return classnames(className, { error: touched && invalid })
+}
+
+RangeInput.defaultProps = defaultProps
+RangeInput.propTypes = propTypes
+
+export default RangeInput
