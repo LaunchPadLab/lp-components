d0516e448bceb855a4f29169a62cec77c914784d
David Pickart <davidstoreypickart@gmail.com>
2017-03-20 16:46:51 -0500

    Add dependency

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,4 @@
-export { default as getDisplayName } from './get-display-name'
-export { default as stripNamespace } from './strip-namespace'
-export { default as toggle } from './toggle'
-
 export { default as startCase } from 'lodash/startCase'
+export { toggle } from '@launchpadlab/lp-utils'
+
+export { default as stripNamespace } from './strip-namespace'
