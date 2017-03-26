a339f5ead3952043225b1c5b8239edc183e58d79
Ryan Francis <ryan.p.francis@gmail.com>
2017-03-26 14:29:27 -0500

    refactor new component

--- src/forms/boolean-link.js
+++ src/forms/boolean-link.js
@@ -1,40 +1,23 @@
 import React, { PropTypes } from 'react'
-import classnames from 'classnames'
 import { fieldPropTypesWithValue } from './field-proptypes'
-import InputError from './input-error'
 
 const propTypes = {
   ...fieldPropTypesWithValue(PropTypes.bool),
-  ...InputError.propTypes,
   label: PropTypes.node.isRequired,
+  className: PropTypes.string,
 }
 
 function BooleanLink ({
-  input,
-  input: { name, value, onChange },
-  meta: { error, touched, invalid },
+  input: { name, onChange },
   label,
-  ...rest
+  className
 }) {
   return (
-    <span className={ classes({ touched, invalid }) }>
-
-      <a
-        id={ name }
-        type="button"        
-        onClick={() => input.onChange(true)}
-        { ...{ name, value, onChange, ...rest } }
-      >{label}</a>      
-
-      <InputError { ...{ error, invalid, touched } } />
-    </span>
-  )
-}
-
-function classes ({ touched, invalid }) {
-  return classnames(
-    'boolean-link',
-    { error: touched && invalid }
+    <a
+      id={ name }
+      onClick={() => onChange(true)}
+      className={ className }
+    >{label}</a>
   )
 }
 
