c6fc6cf3b20a31699ae780d07f12e04c6d0b47ef
David Pickart <davidstoreypickart@gmail.com>
2017-03-29 11:14:38 -0500

    Add default warning

--- src/utils/deprecate.js
+++ src/utils/deprecate.js
@@ -1,13 +1,17 @@
-import { onMount } from '@launchpadlab/lp-utils'
+import { onMount, getDisplayName } from '@launchpadlab/lp-utils'
 
 function displayWarning (warning) {
   // eslint-disable-next-line no-console
   console.warn(`DEPRECATION: ${warning}`)
 }
 
-export default function (warning) {
-  if (!warning || typeof warning !== 'string') throw 'Must include deprecation warning.'
+function defaultMessage (component) {
+  return `${getDisplayName(component)} is deprecated and will be removed in the next version of lp-utils.`
+}
+
+export default function (message) {
   return function (wrappedComponent) {
+    const warning = message || defaultMessage(wrappedComponent)
     return onMount(() => displayWarning(warning))(wrappedComponent)
   }
 }
