05ff9e77fec151ee0384531642c84fd9b66dee08
David Pickart <davidstoreypickart@gmail.com>
2017-04-25 11:32:05 -0500

    Pass tests

--- test/forms/input.test.js
+++ test/forms/input.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { shallow, render } from 'enzyme'
+import { render } from 'enzyme'
 import { Input } from '../../src/'
 
 const name = 'name.of.field'
@@ -42,20 +42,20 @@ test('when error not provided - does not render the error message', () => {
 
 test('if input is invalid but not touched - does not add error class to container', () => {
   const props = { input, meta: { invalid: true, touched: false } }
-  const wrapper = shallow(<Input { ...props }/>)
-  expect(wrapper.find('.error').exists()).toBe(false)
+  const wrapper = render(<Input { ...props }/>)
+  expect(wrapper.find('.error').length).toBe(0)
 })
 
 test('if input is touched but not invalid - does not add error class to container', () => {
   const props = { input, meta: { invalid: false, touched: true } }
-  const wrapper = shallow(<Input { ...props }/>)
-  expect(wrapper.find('.error').exists()).toBe(false)
+  const wrapper = render(<Input { ...props }/>)
+  expect(wrapper.find('.error').length).toBe(0)
 })
 
 test('if input is touched and invalid - adds the error class to container', () => {
   const props = { input, meta: { invalid: true, touched: true } }
-  const wrapper = shallow(<Input { ...props }/>)
-  expect(wrapper.dive().find('.error').exists()).toBe(true)
+  const wrapper = render(<Input { ...props }/>)
+  expect(wrapper.find('.error').length).toBe(1)
 })
 
 test('when error is provided - renders the error message containing the error', () => {
