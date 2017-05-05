7b633b392bc5260d6ccbbe472bda0af2bf8b68fd
David Pickart <davidstoreypickart@gmail.com>
2017-05-05 11:13:22 -0500

    Add docs, stories and tests for SubmitButton

--- src/forms/buttons/submit-button.js
+++ src/forms/buttons/submit-button.js
@@ -1,6 +1,30 @@
 import React from 'react'
 import Button from './button'
 
+/**
+ * A wrapper around the {@link Button} component that adds `type="submit"`. Generally used in the context of forms. 
+ *
+ * With the exception of `type`, this component shares the same props as {@link Button}.
+ *
+ * @name SubmitButton
+ * @type Function
+ * @example
+ * 
+ * function PersonForm ({ handleSubmit, pristine, invalid, submitting }) {
+ *   return (
+ *     <form onSubmit={ handleSubmit }>
+ *       <Field name="name" component={ Input } />
+ *       <Field name="age" component={ Input } type="number" />
+ *       <SubmitButton {...{ pristine, invalid, submitting }}>
+ *         Submit
+ *       </SubmitButton>
+ *     </form>
+ *   )
+ * }
+ * 
+ * // When SubmitButton is pressed, form will submit and handleSubmit() will be called.
+**/
+
 function SubmitButton (props) {
   return (
     <Button { ...props } type="submit"/>  
--- stories/submit-button.story.js
+++ stories/submit-button.story.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import { storiesOf, action } from '@kadira/storybook'
-import { SubmitButton, Submit } from '../src'
+import { Button, SubmitButton, Submit } from '../src'
 
 function submit (e) {
   e.preventDefault()
-  action('submit')(e)
+  action('form submitted')(e)
 }
 
 storiesOf('SubmitButton', module)
   .add('inside form', () => (
     <form onSubmit={submit}>
+      <label> Clicking this button will NOT submit the form (ordinary Button component). </label>
+      <Button> Test </Button>
       <label> Clicking this button will submit the form. </label>
       <SubmitButton> Submit </SubmitButton>
     </form>
