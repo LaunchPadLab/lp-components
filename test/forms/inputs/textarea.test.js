d48f3e286834fa5cd37b2385442a26f59f083e1a
David Pickart <davidstoreypickart@gmail.com>
2017-05-16 13:25:12 -0500

    Add tests

---
+++ test/forms/inputs/textarea.test.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import { Textarea } from '../../../src/'
+
+test('Textarea passes down max length correctly', () => {
+  const props = { 
+    input: {
+      name: 'test',
+      value: '',
+    },
+    meta: {},
+    maxLength: 5,
+  }
+  const wrapper = shallow(<Textarea { ...props }/>)
+  expect(wrapper.dive().find('textarea').prop('maxLength')).toEqual(5)
+})
+
+test('Textarea hides character count correctly', () => {
+  const props = { 
+    input: {
+      name: 'test',
+      value: '',
+    },
+    meta: {},
+    showCharacterCount: false,
+  }
+  const wrapper = shallow(<Textarea { ...props }/>)
+  expect(wrapper.dive().find('.character-count').exists()).toEqual(false)
+})
