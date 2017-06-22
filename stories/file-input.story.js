360095a8269f3dd0aec38a90c83eab3d37f3c9e4
David Pickart <davidstoreypickart@gmail.com>
2017-06-22 11:16:53 -0500

    Add stories

---
+++ stories/file-input.story.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+import { storiesOf, action } from '@kadira/storybook'
+import { FileInput as StaticFileInput } from '../src'
+import dynamicInput from './dynamic-input'
+
+const FileInput = dynamicInput({
+  valuePath: 'input.value',
+  onChangePath: 'input.onChange'
+})(StaticFileInput)
+
+const inputProps = {
+  name: 'person.firstName',
+  onChange: action('field changed')
+}
+
+// Component with filename preview
+class CustomInput extends Component {
+  onLoad (fileData, file) {
+    this.setState({ filename: file.name })
+  }
+  render () {
+    const filename = (this.state || {}).filename
+    return (
+      <FileInput {...{ 
+        onLoad: this.onLoad.bind(this), 
+        ...this.props,
+      }}>
+        <p>{ filename || 'Filename will appear here' }</p>
+      </FileInput>
+    )
+  } 
+}
+
+storiesOf('FileInput', module)
+  .add('with defaults', () => (
+    <FileInput
+      input={inputProps} 
+      meta={{}}
+    />
+  ))
+  .add('with hidden preview', () => (
+    <FileInput
+      input={inputProps}
+      meta={{}}
+      hidePreview
+    />
+  ))
+  .add('with custom preview', () => (
+    <CustomInput
+      input={inputProps}
+      meta={{}}
+    />
+  ))
