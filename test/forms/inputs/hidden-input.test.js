7b71019a9f083e885b2cfc76bb6d7ad2c958d233
Rachel Killackey <rachel@launchpadlab.com>
2017-06-27 13:57:00 -0500

    fix setterlink test, add hiddeninput test

---
+++ test/forms/inputs/hidden-input.test.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { mount } from 'enzyme'
+import { HiddenInput } from '../../../src/'
+
+const name = 'my.hidden.input'
+const value = 'foo'
+const onChange = jest.fn()
+const props = { input: { name, value, onChange }, meta: {} }
+
+test('HiddenInput renders an input', () => {
+  const wrapper = mount(<HiddenInput { ...props }/>)
+  expect(wrapper.find('input').exists()).toEqual(true)
+})
+
+test('HiddenInput renders a div the correct styles', () => {
+  const wrapper = mount(<HiddenInput { ...props }/>)
+  expect(wrapper.find('div').first().props().style).toHaveProperty('position', 'absolute')
+  expect(wrapper.find('div').first().props().style).toHaveProperty('left', -9999)
+})
