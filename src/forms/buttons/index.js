edecb26f7dfa9c65be6f4fb8c6772933da2d1a27
David Pickart <davidstoreypickart@gmail.com>
2017-11-10 11:28:53 -0600

    Remove Submit component (#177)
    * Remove Submit component
    
    * Version bump: 2.0.0

--- src/forms/buttons/index.js
+++ src/forms/buttons/index.js
@@ -1,4 +1,3 @@
 export Button from './button'
 export ButtonArea from './button-area'
-export Submit from './submit'
 export SubmitButton from './submit-button'
--- src/forms/buttons/submit.js
+++
@@ -1,9 +0,0 @@
-import { default as SubmitButton } from './submit-button'
-import { deprecate } from '../../utils'
-
-const warning = 
-`Submit is deprecated and will be removed 
-in the next major version of lp-components. 
-Use SubmitButton instead.`
-
-export default deprecate(warning)(SubmitButton)
