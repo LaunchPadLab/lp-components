fcbb3a74a6fe5d1a25a20b2c4a1587b06db77c0f
inveterateliterate <iribon9@gmail.com>
2017-11-13 10:49:32 -0600

    default maxLength to false (#179)
    * default maxLength to false - no max length
    
    * change showCharacterCount to hideCharacterCount
    
    * update documentation

--- test/forms/inputs/textarea.test.js
+++ test/forms/inputs/textarea.test.js
@@ -2,7 +2,20 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import { Textarea } from '../../../src/'
 
-test('Textarea passes down max length correctly', () => {
+test('Textarea passes down defaults and does not show character count', () => {
+  const props = {
+    input: {
+      name: 'test',
+      value: '',
+    },
+    meta: {},
+  }
+  const wrapper = shallow(<Textarea { ...props }/>)
+  expect(wrapper.dive().find('textarea').prop('maxLength')).toEqual(false)
+  expect(wrapper.dive().find('.character-count').exists()).toEqual(false)
+})
+
+test('Textarea passes down max length and shows character count correctly', () => {
   const props = {
     input: {
       name: 'test',
@@ -13,6 +26,7 @@ test('Textarea passes down max length correctly', () => {
   }
   const wrapper = shallow(<Textarea { ...props }/>)
   expect(wrapper.dive().find('textarea').prop('maxLength')).toEqual(5)
+  expect(wrapper.dive().find('.character-count').exists()).toEqual(true)
 })
 
 test('Textarea hides character count correctly', () => {
@@ -22,23 +36,12 @@ test('Textarea hides character count correctly', () => {
       value: '',
     },
     meta: {},
-    showCharacterCount: false,
+    maxLength: 5,
+    hideCharacterCount: true,
   }
   const wrapper = shallow(<Textarea { ...props }/>)
+  expect(wrapper.dive().find('textarea').prop('maxLength')).toEqual(5)
   expect(wrapper.dive().find('.character-count').exists()).toEqual(false)
 })
 
-test('Textarea passes down max length and hides character count correctly', () => {
-  const props = {
-    input: {
-      name: 'test',
-      value: '',
-    },
-    meta: {},
-    maxLength: false,
-  }
-  const wrapper = shallow(<Textarea { ...props }/>)
-  expect(wrapper.dive().find('textarea').prop('maxLength')).toEqual(false)
-  expect(wrapper.dive().find('.character-count').exists()).toEqual(false)
-})
 
