dad4bced61670b2394e02e7b840b767c72c621e2
David Pickart <davidstoreypickart@gmail.com>
2018-04-03 13:59:57 -0500

    More tests

--- test/controls/color-picker.test.js
+++ test/controls/color-picker.test.js
@@ -2,23 +2,19 @@ import React from 'react'
 import { mount } from 'enzyme'
 import { ColorPicker } from '../../src/'
 
-test('ColorPicker expands when swatch is clicked', () => {
+test('ColorPicker toggles expanded when swatch is clicked', () => {
   const wrapper = mount(<ColorPicker />)
   expect(wrapper.find('.popover').exists()).toBe(false)
   wrapper.find('.swatch').simulate('click')
   expect(wrapper.find('.popover').exists()).toBe(true)
+  wrapper.find('.swatch').simulate('click')
+  expect(wrapper.find('.popover').exists()).toBe(false)
 })
 
-// test('ColorInput collapses when blurred', () => {
-//   const props = { 
-//     input: {
-//       name: 'test',
-//       value: '',
-//     }, 
-//     meta: {},
-//   }
-//   const wrapper = mount(<ColorInput { ...props }/>)
-//   expect(wrapper.find('.popover').exists()).toBe(false)
-//   wrapper.find('.hex-input').simulate('focus')
-//   expect(wrapper.find('.popover').exists()).toBe(true)
-// })
+test('ColorPicker collapses when background is clicked', () => {
+  const wrapper = mount(<ColorPicker />)
+  wrapper.find('.swatch').simulate('click')
+  expect(wrapper.find('.popover').exists()).toBe(true)
+  wrapper.find('.cover').simulate('click')
+  expect(wrapper.find('.popover').exists()).toBe(false)
+})
--- test/setup.js
+++ test/setup.js
@@ -4,3 +4,5 @@ import Adapter from 'enzyme-adapter-react-16'
 // Configure enzyme
 enzyme.configure({ adapter: new Adapter() })
 
+// Mock browser canvas
+import 'jest-canvas-mock'
