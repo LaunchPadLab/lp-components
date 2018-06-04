adf9c9aa5e81a523cd392212062335b253576862
David Pickart <davidstoreypickart@gmail.com>
2018-06-04 12:09:12 -0500

    Allow conditional rendering of columns (#244)
    * Ignore columns without props
    
    * Upgrade deps
    
    * Version bump: 3.11.0
    
    * Patch instead of minor

--- src/tables/helpers/get-column-data.js
+++ src/tables/helpers/get-column-data.js
@@ -1,8 +1,11 @@
+import { castArray, has } from '../../utils'
 
 // Get column info from children via props
 function getColumnData (children=[], doDisable) {
-  const childrenArray = Array.isArray(children) ? children : [children]
-  return childrenArray.map(({ props }) => {
+  const childrenArray = castArray(children)
+  return childrenArray
+    .filter(child => has(child, 'props'))
+    .map(({ props }) => {
     // If sort is disabled, disable all columns
     const disabled = doDisable || props.disabled || false
     return { ...props, disabled }
