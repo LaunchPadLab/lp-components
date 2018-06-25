b42a1941909f15a9a2f6f3ea9031fc63fe5b9a30
Rachel Killackey <rachel@launchpadlab.com>
2018-06-25 13:58:15 -0500

    add docs

--- src/controls/nav-link.js
+++ src/controls/nav-link.js
@@ -2,6 +2,26 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router'
 
+/**
+ * A component that wraps the `react-router` [`Link`](https://github.com/ReactTraining/react-router/blob/master/packages/react-router-dom/docs/api/Link.md) component, adding an `activeClassName`.
+ * A full list of props supported by this component can be found [here](https://github.com/ReactTraining/react-router/blob/master/packages/react-router-dom/docs/api/Link.md).
+ * 
+ * @name NavLink
+ * @type Function
+ * @description 
+ * @example
+ *
+ * function Header () {
+ *   return (
+ *     <ul>
+ *      <li><NavLink to="/">Home</NavLink></li>
+ *      <li><NavLink to="/blog">Blog</NavLink></li>
+ *     </ul>
+ *   )
+ * }
+ * 
+**/
+
 const propTypes = {
   children: PropTypes.node,
 }
