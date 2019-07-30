cfd84d99c031782982fabf9ae2fbb44c5e645453
David Pickart <davidstoreypickart@gmail.com>
2019-07-30 14:27:15 -0500

    Pass message props to flash component (#370)
    * Pass message props to flash component
    
    * Version bump: 3.25.0

--- test/indicators/flash-message.test.js
+++ test/indicators/flash-message.test.js
@@ -3,6 +3,7 @@ import { mount } from 'enzyme'
 import { FlashMessage } from '../../src/'
 
 const successMessage = { id: '0', message: 'Success!', isError: false, props: {} }
+const customMessage = { id: '0', message: 'Success!', isError: false, props: { className: 'foo', hidden: true } }
 
 test('FlashMessage only shows dismiss button when callback is provided', () => {
   const wrapper = mount(
@@ -13,4 +14,12 @@ test('FlashMessage only shows dismiss button when callback is provided', () => {
     <FlashMessage message={ successMessage } onDismiss={ () => { /* do something */ } } />
   )
   expect(dismissWrapper.find('button.dismiss').exists()).toBe(true)
+})
+
+test('FlashMessage accepts props from message object', () => {
+  const wrapper = mount(
+    <FlashMessage message={ customMessage } />
+  )
+  expect(wrapper.find('div.foo').exists()).toBe(true)
+  expect(wrapper.find('div').prop('hidden')).toBe(true)
 })
