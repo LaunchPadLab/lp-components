e07230f9d9564af1a58445c201e7d8d0ea80af9e
David Pickart <davidstoreypickart@gmail.com>
2019-08-23 15:35:43 -0500

    Upgrade dev dependencies (#375)
    * Upgrade dev dependencies
    
    * Version bump: 3.25.2

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -3,31 +3,29 @@ const bourbonIncludePaths = require('bourbon').includePaths
 const neatIncludePaths = require('bourbon-neat').includePaths
 const datePickerIncludePaths = [ path.resolve(__dirname, '../node_modules/react-datepicker/src/stylesheets') ]
 
-module.exports = {
-  module: {
-    rules: [
+module.exports = async ({ config }) => {
+  config.module.rules.push({
+    test: /\.scss$/,
+    use: [
+      'style-loader',
+      'css-loader',
       {
-        test: /\.scss$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          {
-            loader: 'sass-loader',
-            options: {
-              includePaths: [
-                datePickerIncludePaths,
-                bourbonIncludePaths,
-                neatIncludePaths,
-              ]
-            }
-          }
-        ]
-      },
+        loader: 'sass-loader',
+        options: {
+          includePaths: [
+            datePickerIncludePaths,
+            bourbonIncludePaths,
+            neatIncludePaths,
+          ]
+        }
+      }
     ]
-  },
-  resolve: {
+  })
+  config.resolve = {
+    ...config.resolve,
     alias: {
       'src': path.resolve(__dirname, '../src')
     }
   }
+  return config
 }
