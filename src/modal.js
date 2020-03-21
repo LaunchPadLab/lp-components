2be8316b5ede06679ef0ad5d71cbca75d5541131
David Pickart <davidstoreypickart@gmail.com>
2020-03-20 19:26:31 -0500

    wip

---
+++ src/modal.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import ReactModal from 'react-modal'
+
+const propTypes = {
+  shown: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+  hideCloseButton: PropTypes.bool,
+  children: PropTypes.node,
+}
+
+const defaultProps = {
+  hideCloseButton: false,
+}
+
+function getRootElement() {
+  // Skip in SSR mode
+  if (typeof window === 'undefined') return
+  // Note that this expects a root element with id "root"
+  // eslint-disable-next-line no-undef
+  return window.document.querySelector('#root')
+}
+
+// A wrapper for react-modal that adds some styles and a close button.
+// See https://github.com/reactjs/react-modal for usage.
+function Modal({ shown, onClose, hideCloseButton, children, ...rest }) {
+  return (
+    <ReactModal
+      isOpen
+      onRequestClose={onClose}
+      portalClassName="modal"
+      className="modal-inner"
+      overlayClassName="modal-fade-screen"
+      bodyOpenClassName="modal-open"
+      appElement={getRootElement()}
+      {...rest}
+    >
+      <div className="modal-content rich-text">{children}</div>
+      {!hideCloseButton && (
+        <button
+          onClick={onClose}
+          className="close"
+          aria-label="Close Modal"
+        ></button>
+      )}
+      {!hideCloseButton && (
+        <button onClick={onClose} className="button-primary">
+          Close
+        </button>
+      )}
+    </ReactModal>
+  )
+}
+
+Modal.propTypes = propTypes
+Modal.defaultProps = defaultProps
+
+export default Modal
