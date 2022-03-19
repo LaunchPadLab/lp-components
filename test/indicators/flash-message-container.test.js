3b14b867aae94258616fdea361a501cb2b1cff3c
Conor <conor@launchpadlab.com>
2022-03-19 14:23:47 -0400

    Pass redux flash message to dismiss fn (#522)

--- test/indicators/flash-message-container.test.js
+++ test/indicators/flash-message-container.test.js
@@ -34,3 +34,41 @@ test('FlashMessageContainer displays all provided redux-flash messages', () => {
       .contains('Failure!')
   ).toBe(true)
 })
+
+test('FlashMessageContainer passes down additional props to each message', () => {
+  const wrapper = mount(
+    <FlashMessageContainer
+      messages={[successMessage, failureMessage]}
+      data-test="flash"
+    />
+  )
+  expect(wrapper.find('div.flash-message[data-test="flash"]').length).toEqual(2)
+})
+
+test('FlashMessageContainer props get overridden by message props', () => {
+  const specialFailureMessage = {
+    ...failureMessage,
+    props: { 'data-test': 'error-flash' }
+  }
+  const wrapper = mount(
+    <FlashMessageContainer
+      messages={[successMessage, specialFailureMessage]}
+      data-test="flash"
+    />
+  )
+  expect(wrapper.find('div.flash-message[data-test="flash"]').length).toEqual(1)
+  expect(wrapper.find('div.flash-message[data-test="error-flash"]').length).toEqual(1)
+})
+
+test('FlashMessageContainer onDismiss gets invoked with message', () => {
+  const onDismiss = jest.fn()
+  const wrapper = mount(
+    <FlashMessageContainer
+      messages={[successMessage, failureMessage]}
+      onDismiss={onDismiss}
+    />
+  )
+  wrapper.find('div.flash-message.success > button').simulate('click')
+  expect(onDismiss).toHaveBeenCalledTimes(1)
+  expect(onDismiss).toHaveBeenCalledWith(successMessage)
+})
