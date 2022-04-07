94a5e86b9bdd5da9632cbcfdcf6d6a6804c00b7a
Mike Wislek <mike@launchpadlab.com>
2022-04-07 12:08:59 -0500

    chore: upgrade all babel-related dependencies

--- test/setup.js
+++ test/setup.js
@@ -1,3 +1,7 @@
+// Required since lp-hoc > lp-requests uses async/await
+import 'core-js/stable'
+import 'regenerator-runtime/runtime.js'
+
 import enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
@@ -7,5 +11,3 @@ enzyme.configure({ adapter: new Adapter() })
 // Mock browser canvas
 import 'jest-canvas-mock'
 
-// Add babel polyfill since lp-hoc > lp-requests uses async/await
-import '@babel/polyfill'
