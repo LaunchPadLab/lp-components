89783c2f2cb257e70bfe3740263ca3378589f5f5
Conor Hawes <conor@launchpadlab.com>
2022-06-15 13:51:30 -0400

    Be more specific

--- src/forms/helpers/field-prop-types.js
+++ src/forms/helpers/field-prop-types.js
@@ -138,5 +138,5 @@ const file = PropTypes.shape({
 })
 
 export const fileInputPropTypes = fieldPropTypesWithValue(
-  PropTypes.oneOfType([PropTypes.object, PropTypes.arrayOf(file)])
+  PropTypes.oneOfType([file, PropTypes.arrayOf(file)])
 )
