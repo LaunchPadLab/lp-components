016a81d59fb62a4a8489f3510148c615c25d5b6b
Conor <conor@launchpadlab.com>
2022-06-15 16:55:22 -0400

    Fix tests and prop types (#553)
    * Hotfix
    
    * Be more specific
    
    * Fix select test warnings
    
    * Fix textarea test warnings
    
    * Fix file input test warnings
    
    * Dedupe yarn.lock file to help resolve issues

--- src/forms/helpers/field-prop-types.js
+++ src/forms/helpers/field-prop-types.js
@@ -138,5 +138,5 @@ const file = PropTypes.shape({
 })
 
 export const fileInputPropTypes = fieldPropTypesWithValue(
-  PropTypes.arrayOf(file).isRequired,
+  PropTypes.oneOfType([PropTypes.oneOf([""]), file, PropTypes.arrayOf(file)])
 )
