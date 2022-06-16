1f7df628a2d6980df3219026879d8c9c0b649a7b
Conor Hawes <conor@launchpadlab.com>
2022-06-16 09:35:15 -0400

    Add specs

--- test/modal.test.js
+++ test/modal.test.js
@@ -2,8 +2,19 @@ import React from 'react'
 import { mount } from 'enzyme'
 import { Modal } from '../src/'
 import { noop } from 'lodash'
+import ReactModal from 'react-modal'
 
 describe('Modal', () => {
+  // requestAnimationFrame is async, so the callback fails to trigger
+  // https://github.com/reactjs/react-modal/issues/903
+  beforeEach(() => {
+    jest.spyOn(window, 'requestAnimationFrame').mockImplementation(cb => cb());
+  });
+  
+  afterEach(() => {
+    window.requestAnimationFrame.mockRestore();
+  });
+
   test('is shown by default', () => {
     const wrapper = mount(<Modal onClose={noop} />)
     expect(wrapper.find('.modal-content').exists()).toEqual(true)
@@ -19,6 +30,26 @@ describe('Modal', () => {
     const wrapper = mount(<Modal onClose={onClose} />)
     wrapper.find('.modal-close').simulate('click')
     expect(onClose).toHaveBeenCalled()
+  })
+
+  test('adds additional class string to default class', () => {
+    const wrapper = mount(<Modal onClose={noop} className="custom" />)
+    expect(wrapper.find('.modal-inner.custom').exists()).toEqual(true)
+  })
+
+  test('adds additional overlay class string to default overlay class', () => {
+    const wrapper = mount(<Modal onClose={noop} overlayClassName="custom-overlay" />)
+    expect(wrapper.find('.modal-fade-screen.custom-overlay').exists()).toEqual(true)
+  })
+
+  test('adds additional class object to default class', () => {
+    const wrapper = mount(<Modal isOpen={true} onClose={noop} className={{ base: 'custom', afterOpen: 'modal-is-open', beforeClose: 'modal-before-close' }} />)
+    expect(wrapper.find('.modal-inner.custom.modal-is-open').exists()).toEqual(true)
+  })
+
+  test('adds additional overlay class object to default overlay class', () => {
+    const wrapper = mount(<Modal isOpen={true} onClose={noop} overlayClassName={{ base: 'custom', afterOpen: 'modal-is-open', beforeClose: 'modal-before-close' }} />)
+    expect(wrapper.find('.modal-fade-screen.custom.modal-is-open').exists()).toEqual(true)
   })
 
   describe('when preventClose=true', () => {
