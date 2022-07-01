f2923c94e2ab4ba3a9b331a3de9ef8a4ab1ad94f
Conor Hawes <conor@launchpadlab.com>
2022-07-01 17:58:57 -0400

    Coerce using Boolean constructor

--- src/utils/local/use-toggle.js
+++ src/utils/local/use-toggle.js
@@ -4,7 +4,10 @@ function useToggle (defaultValue = false) {
   const [value, setValue] = useState(defaultValue)
   const toggle = useCallback((override) => {
     setValue((currentValue) => {
-      return override === undefined ? !currentValue : !!override
+      // default behavior should set the opposite value
+      if (override === undefined) return !currentValue
+
+      return Boolean(override)
     })
   }, [])
 
