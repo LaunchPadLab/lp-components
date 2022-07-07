eb5a6fa65e3e535aa0797141a2da5de585e412eb
Conor <conor@launchpadlab.com>
2022-07-07 09:30:42 -0400

    Hotfix: Fix storybook build (#573)
    * Use recommended file for configuration
    
    * Bump patch

---
+++ .storybook/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+  stories: ['../**/*.story.js'],
+  addons: [
+    '@storybook/addon-actions',
+    '@storybook/addon-a11y',
+    '@storybook/addon-storysource',
+    '@storybook/addon-viewport'
+  ],
+}
