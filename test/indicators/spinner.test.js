9b16899aa76cfa7f6a4ed37b98cb6b0b98deca33
Josias Schneider <josias.ds@gmail.com>
2023-03-13 15:28:29 -0300

    Migrate Spinner

--- test/indicators/spinner.test.js
+++ test/indicators/spinner.test.js
@@ -1,25 +1,13 @@
 import React from 'react'
-import { mount } from 'enzyme'
+import { render, screen } from '@testing-library/react'
 import { Spinner } from '../../src/'
 
-test('Spinner creates div with class "spinner"', () => {
-  const wrapper = mount(<Spinner />)
-  expect(wrapper.find('div.spinner').exists()).toBe(true)
+test('Spinner has a defined role', () => {
+  render(<Spinner />)
+  expect(screen.getByRole('progressbar')).toBeInTheDocument()
 })
 
-test('Spinner can have custom className', () => {
-  const wrapper = mount(<Spinner className="custom" />)
-  expect(wrapper.find('div.spinner.custom').exists()).toBe(true)
-})
-
-test('Spinner passes props', () => {
-  const wrapper = mount(<Spinner name="Bob" />)
-  expect(wrapper.find('div.spinner').props().name).toEqual('Bob')
-})
-
-test('Spinner filters invalid props', () => {
-  const wrapper = mount(
-    <Spinner name="Bob" someInvalidProp="I am not a valid DOM prop" />
-  )
-  expect(wrapper.find('div.spinner').props().someInvalidProp).not.toBeDefined()
+test('Spinner can have a custom className', () => {
+  render(<Spinner className="custom" />)
+  expect(screen.getByRole('progressbar')).toHaveClass('custom')
 })
