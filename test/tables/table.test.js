aee79df1d788e11735ff078e9c7878438380893b
Conor Hawes <conor@launchpadlab.com>
2023-08-04 16:10:02 -0400

    Revert hack by using role

--- test/tables/table.test.js
+++ test/tables/table.test.js
@@ -3,18 +3,18 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { Table, TableColumn as Column } from '../../src/'
 
-const tableData = [{ name: 'Kim' }, { name: 'Tommy' }, { name: 'Kam' }]
+const tableData = [{ name: 'Lorax' }, { name: 'Tommy' }, { name: 'Kim' }]
 
 test('Table does not sort data', async () => {
   const user = userEvent.setup()
   render(
-    <Table data={tableData} >
+    <Table data={tableData}>
       <Column name="name" />
     </Table>
   )
   await user.click(screen.getByText('Name'))
   // Data remains unsorted
-  const cells = screen.getAllByText(/^K/)
-  expect(cells.at(0).textContent).toBe('Kim')
-  expect(cells.at(1).textContent).toBe('Kam')
+  const cells = screen.getAllByRole('cell')
+  expect(cells.at(0).textContent).toBe('Lorax')
+  expect(cells.at(-1).textContent).toBe('Kim')
 })
