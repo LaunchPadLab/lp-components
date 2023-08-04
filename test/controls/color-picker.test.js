896d99d6bd76bd96ee713df4a28a2d2ed594a5ce
Conor Hawes <conor@launchpadlab.com>
2023-08-04 11:36:11 -0400

    Add comment

--- test/controls/color-picker.test.js
+++ test/controls/color-picker.test.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { mount } from 'enzyme'
 import { ColorPicker } from '../../src/'
 
+// TODO: Migrate after incoming change to use a button instead of a div
+// https://github.com/LaunchPadLab/lp-components/pull/596
+
 test('ColorPicker toggles expanded when swatch is clicked', () => {
   const wrapper = mount(<ColorPicker />)
   expect(wrapper.find('.popover').exists()).toBe(false)
