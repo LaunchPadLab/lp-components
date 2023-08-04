b279bc3d3e1c9953f720bd1d7cfc7dc263221ddc
Conor Hawes <conor@launchpadlab.com>
2023-08-04 12:01:51 -0400

    Remove unused import

--- test/controls/paginator/paginator.test.js
+++ test/controls/paginator/paginator.test.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import { mount } from 'enzyme'
 import { Paginator } from '../../../src/'
 
 test('Previous control renders unless value is min', () => {
