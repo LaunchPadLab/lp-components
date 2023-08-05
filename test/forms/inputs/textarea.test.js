9edbec0782a381d7483ff38c05a17cb31a4ead78
Conor Hawes <conor@launchpadlab.com>
2023-08-04 23:26:40 -0400

    Migrate textarea

--- test/forms/inputs/textarea.test.js
+++ test/forms/inputs/textarea.test.js
@@ -1,5 +1,5 @@
 import React, { createRef } from 'react'
-import { mount } from 'enzyme'
+import { render, screen } from '@testing-library/react'
 import { Textarea } from '../../../src/'
 
 const onChange = () => {}
@@ -13,9 +13,10 @@ test('Textarea passes down defaults and does not show character count', () => {
     },
     meta: {},
   }
-  const wrapper = mount(<Textarea {...props} />)
-  expect(wrapper.find('textarea').prop('maxLength')).toEqual(null)
-  expect(wrapper.find('.character-count').exists()).toEqual(false)
+  render(<Textarea {...props} />)
+  const textarea = screen.getByLabelText('Test')
+  expect(textarea).not.toHaveAttribute('maxLength')
+  expect(textarea.previousSibling).not.toHaveClass('character-count')
 })
 
 test('Textarea passes down max length and shows character count correctly', () => {
@@ -28,9 +29,9 @@ test('Textarea passes down max length and shows character count correctly', () =
     meta: {},
     maxLength: 5,
   }
-  const wrapper = mount(<Textarea {...props} />)
-  expect(wrapper.find('textarea').prop('maxLength')).toEqual(5)
-  expect(wrapper.find('.character-count').exists()).toEqual(true)
+  render(<Textarea {...props} />)
+  expect(screen.getByLabelText('Test')).toHaveAttribute('maxLength', '5')
+  expect(screen.getByText('0/5 characters'))
 })
 
 test('Textarea hides character count correctly', () => {
@@ -44,9 +45,9 @@ test('Textarea hides character count correctly', () => {
     maxLength: 5,
     hideCharacterCount: true,
   }
-  const wrapper = mount(<Textarea {...props} />)
-  expect(wrapper.find('textarea').prop('maxLength')).toEqual(5)
-  expect(wrapper.find('.character-count').exists()).toEqual(false)
+  render(<Textarea {...props} />)
+  expect(screen.getByLabelText('Test')).toHaveAttribute('maxLength', '5')
+  expect(screen.queryByText('0/5 characters')).not.toBeInTheDocument()
 })
 
 test('Textarea is given an aria-describedby attribute when there is an input error', () => {
@@ -64,8 +65,8 @@ test('Textarea is given an aria-describedby attribute when there is an input err
     maxLength: 5,
     hideCharacterCount: true,
   }
-  const wrapper = mount(<Textarea {...props} />)
-  expect(wrapper.find('textarea').prop('aria-describedby')).toContain(name)
+  render(<Textarea {...props} />)
+  expect(screen.getByLabelText('Test').getAttribute('aria-describedby')).toContain(name)
 })
 
 test('Textarea does not receive invalid dom attributes', () => {
@@ -82,8 +83,8 @@ test('Textarea does not receive invalid dom attributes', () => {
     onClickLabel: () => 'foo',
   }
 
-  const wrapper = mount(<Textarea {...props} />)
-  expect(wrapper.find('textarea').prop('onClickLabel')).toBe(undefined)
+  render(<Textarea {...props} />)
+  expect(screen.getByLabelText('Test')).not.toHaveAttribute('onClickLabel')
 })
 
 test('Textarea passes down forwardedRef to input correctly', () => {
@@ -98,6 +99,6 @@ test('Textarea passes down forwardedRef to input correctly', () => {
     forwardedRef: inputRef,
   }
 
-  const wrapper = mount(<Textarea {...props} />)
-  expect(wrapper.find('textarea').prop('id')).toEqual(inputRef.current.id)
+  render(<Textarea {...props} />)
+  expect(screen.getByLabelText('Test').id).toEqual(inputRef.current.id)
 })
