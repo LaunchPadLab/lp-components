495b277dd0aa5f0bc53a6b79472b9e81d5570a06
Conor Hawes <conor@launchpadlab.com>
2023-08-04 23:30:55 -0400

    Remove unused import

--- test/forms/inputs/switch.test.js
+++ test/forms/inputs/switch.test.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import { mount } from 'enzyme'
 import { Switch } from '../../../src'
 
 test('Switch toggles value when clicked', async () => {
