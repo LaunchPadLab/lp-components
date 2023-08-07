ff0d519f0c5bfd91cf155b9181a1717f97f6b4e9
Conor Hawes <conor@launchpadlab.com>
2023-08-07 10:03:32 -0400

    Migrate radio group input

--- test/forms/inputs/radio-group.test.js
+++ test/forms/inputs/radio-group.test.js
@@ -1,8 +1,10 @@
 import React from 'react'
-import { mount } from 'enzyme'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { RadioGroup } from '../../../src/'
 
-test('RadioGroup changes value when buttons are clicked', () => {
+test('RadioGroup changes value when buttons are clicked', async () => {
+  const user = userEvent.setup()
   const onChange = jest.fn()
   const props = {
     input: {
@@ -13,10 +15,11 @@ test('RadioGroup changes value when buttons are clicked', () => {
     meta: {},
     options: ['Option 1', 'Option 2'],
   }
-  const wrapper = mount(<RadioGroup {...props} />)
-  wrapper.find('input').first().simulate('change')
+  render(<RadioGroup {...props} />)
+  const radioOptions = screen.getAllByRole('radio')
+  await user.click(radioOptions.at(0))
   expect(onChange).toHaveBeenCalledWith('Option 1')
-  wrapper.find('input').last().simulate('change')
+  await user.click(radioOptions.at(1))
   expect(onChange).toHaveBeenCalledWith('Option 2')
 })
 
@@ -30,26 +33,28 @@ test("RadioGroup's inputs all have the same name", () => {
     meta: {},
     options: ['Option 1', 'Option 2'],
   }
-  const wrapper = mount(<RadioGroup {...props} />)
-  expect(wrapper.find('input').first().prop('name')).toEqual(name)
-  expect(wrapper.find('input').last().prop('name')).toEqual(name)
+  render(<RadioGroup {...props} />)
+  const radioOptions = screen.getAllByRole('radio')
+  expect(radioOptions.at(0)).toHaveAttribute('name', name)
+  expect(radioOptions.at(1)).toHaveAttribute('name', name)
 })
 
 test("RadioGroup input has a value that matches the corresponding option's value", () => {
+  const options = ['Option 1', 'Option 2']
   const props = {
     input: {
       name: 'test',
       value: '',
     },
     meta: {},
-    options: ['Option 1', 'Option 2'],
+    options,
   }
-  const wrapper = mount(<RadioGroup {...props} />)
-  expect(wrapper.find('input').first().prop('value')).toEqual('Option 1')
-  expect(wrapper.find('input').last().prop('value')).toEqual('Option 2')
+  render(<RadioGroup {...props} />)
+  expect(screen.getByRole('radio', { name: options.at(0) })).toHaveAttribute('value', options.at(0))
+  expect(screen.getByRole('radio', { name: options.at(1) })).toHaveAttribute('value', options.at(1))
 })
 
-test("RadioGroup has a legend with the group's name by default", () => {
+test("RadioGroup has a legend with the input's name start-cased by default", () => {
   const name = 'sameName'
   const props = {
     input: {
@@ -59,10 +64,8 @@ test("RadioGroup has a legend with the group's name by default", () => {
     meta: {},
     options: ['Option 1', 'Option 2'],
   }
-  const wrapper = mount(<RadioGroup {...props} />)
-  const legend = wrapper.find('fieldset').first().find('legend')
-  expect(legend).toBeTruthy()
-  expect(legend.text()).toEqual('Same Name')
+  render(<RadioGroup {...props} />)
+  expect(screen.getByRole('group', { name: 'Same Name' })).toBeInTheDocument()
 })
 
 test("RadioGroup has a legend with the group's label (when provided)", () => {
@@ -76,42 +79,40 @@ test("RadioGroup has a legend with the group's label (when provided)", () => {
     label: 'Different Name',
     options: ['Option 1', 'Option 2'],
   }
-  const wrapper = mount(<RadioGroup {...props} />)
-  const legend = wrapper.find('fieldset').first().find('legend')
-  expect(legend).toBeTruthy()
-  expect(legend.text()).toEqual('Different Name')
+  render(<RadioGroup {...props} />)
+  expect(screen.getByRole('group', { name: 'Different Name' })).toBeInTheDocument()
 })
 
 test('RadioGroup does not pass down class name', () => {
-  const onChange = jest.fn()
   const props = {
     input: {
       name: 'test',
       value: '',
-      onChange,
     },
     meta: {},
-    options: ['Option 1', 'Option 2'],
+    options: ['Option 1'],
     className: 'custom-radio',
   }
-  const wrapper = mount(<RadioGroup {...props} />)
-  expect(wrapper.find('.custom-radio').hostNodes().length).toEqual(1)
+  render(<RadioGroup {...props} />)
+  expect(screen.getByRole('group', { name: 'Test' })).toHaveClass('custom-radio')
+  expect(screen.getByRole('radio')).not.toHaveClass('custom-radio')
 })
 
 test('RadioGroup passes down props to children', () => {
-  const onChange = jest.fn()
   const props = {
     input: {
       name: 'test',
       value: '',
-      onChange,
     },
     meta: {},
     options: ['Option 1', 'Option 2'],
     className: 'custom-radio-group',
+    'data-test': 'true',
     radioInputProps: { className: 'custom-radio-input' },
   }
-  const wrapper = mount(<RadioGroup {...props} />)
-  expect(wrapper.find('input.custom-radio-group').exists()).toBe(false)
-  expect(wrapper.find('input.custom-radio-input').exists()).toBe(true)
+  render(<RadioGroup {...props} />)
+  screen.getAllByRole('radio').forEach((el) => {
+    expect(el).toHaveClass('custom-radio-input')
+    expect(el).toHaveAttribute('data-test', 'true')
+  })
 })
