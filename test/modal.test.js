78f76ac99f4f4ae1495c5a73875fba56d5131f7f
Conor Hawes <conor@launchpadlab.com>
2023-08-07 11:41:14 -0400

    Add tests more default close interactions

--- test/modal.test.js
+++ test/modal.test.js
@@ -24,13 +24,21 @@ describe('Modal', () => {
 
   test('is shown by default', () => {
     const content = Date.now()
-    render(<MyModal onClose={noop}><span>{content}</span></MyModal>)
+    render(
+      <MyModal onClose={noop}>
+        <span>{content}</span>
+      </MyModal>
+    )
     expect(screen.getByText(content)).toBeInTheDocument()
   })
 
   test('can be hidden/animated by manually passing isOpen', () => {
     const content = Date.now()
-    render(<MyModal isOpen={false} onClose={noop}><span>{content}</span></MyModal>)
+    render(
+      <MyModal isOpen={false} onClose={noop}>
+        <span>{content}</span>
+      </MyModal>
+    )
     expect(screen.queryByText(content)).not.toBeInTheDocument()
   })
 
@@ -43,15 +51,31 @@ describe('Modal', () => {
     expect(onClose).toHaveBeenCalled()
   })
 
+  test('calls close handler when escape key is pressed', async () => {
+    const onClose = jest.fn()
+    const user = userEvent.setup()
+    render(<MyModal onClose={onClose} />)
+
+    await user.keyboard('{Escape}')
+    expect(onClose).toHaveBeenCalled()
+  })
+
+  test('calls close handler when overlay is clicked', async () => {
+    const onClose = jest.fn()
+    const user = userEvent.setup()
+    render(<MyModal onClose={onClose} />)
+    const overlay = screen.getByRole('dialog').parentElement
+    await user.click(overlay)
+    expect(onClose).toHaveBeenCalled()
+  })
+
   test('adds additional class string to default class', () => {
     render(<MyModal onClose={noop} className="custom" />)
     expect(screen.getByRole('dialog')).toHaveClass('modal-inner', 'custom')
   })
 
   test('adds additional overlay class string to default overlay class', () => {
-    render(
-      <MyModal onClose={noop} overlayClassName="custom-overlay" />
-    )
+    render(<MyModal onClose={noop} overlayClassName="custom-overlay" />)
 
     const overlay = screen.getByRole('dialog').parentElement
     expect(overlay).toHaveClass('modal-fade-screen', 'custom-overlay')
@@ -69,7 +93,11 @@ describe('Modal', () => {
         }}
       />
     )
-    expect(screen.getByRole('dialog')).toHaveClass('modal-inner', 'custom', 'modal-is-open')
+    expect(screen.getByRole('dialog')).toHaveClass(
+      'modal-inner',
+      'custom',
+      'modal-is-open'
+    )
   })
 
   test('adds additional overlay class object to default overlay class', () => {
@@ -91,7 +119,9 @@ describe('Modal', () => {
   describe('when preventClose=true', () => {
     test('hides close button', () => {
       render(<MyModal preventClose={true} onClose={noop} />)
-      expect(screen.queryByRole('button', { name: 'Close Modal' })).not.toBeInTheDocument()
+      expect(
+        screen.queryByRole('button', { name: 'Close Modal' })
+      ).not.toBeInTheDocument()
     })
 
     test('does not close by escape key', async () => {
@@ -106,12 +136,7 @@ describe('Modal', () => {
     test('does not close by overlay click', async () => {
       const onClose = jest.fn()
       const user = userEvent.setup()
-      render(
-        <MyModal
-          preventClose={true}
-          onClose={onClose}
-        />
-      )
+      render(<MyModal preventClose={true} onClose={onClose} />)
       const overlay = screen.getByRole('dialog').parentElement
       await user.click(overlay)
       expect(onClose).not.toHaveBeenCalled()
@@ -121,7 +146,11 @@ describe('Modal', () => {
       const onClose = jest.fn()
       const user = userEvent.setup()
       render(
-        <MyModal preventClose={true} onClose={onClose} shouldCloseOnEsc={true} />
+        <MyModal
+          preventClose={true}
+          onClose={onClose}
+          shouldCloseOnEsc={true}
+        />
       )
       await user.keyboard('{Escape}')
       expect(onClose).toHaveBeenCalled()
