e846098cc950d1d8858734992d8bb7a6566449db
Josias Schneider <josias.ds@gmail.com>
2023-08-15 10:53:19 -0300

    Remove moment dependency from DateInput test

--- test/forms/inputs/date-input.test.js
+++ test/forms/inputs/date-input.test.js
@@ -1,13 +1,11 @@
 import React, { useState } from 'react'
 import { DateInput } from '../../../src/'
 import { render, screen } from '@testing-library/react'
-import { within } from '@testing-library/dom'
 import userEvent from '@testing-library/user-event'
-import moment from 'moment'
 
 const name = 'name.of.field'
 const value = '2020-01-01'
-const noop = () => { }
+const noop = () => {}
 const input = { name, value }
 const error = 'input error'
 
@@ -29,18 +27,18 @@ const WrappedDateInput = (props) => {
 
 test('DateInput renders the error message when provided', () => {
   const props = { input, meta: { invalid: true, touched: true, error } }
-  const { container } = render(<WrappedDateInput {...props} />)
-  expect(within(container).getByText('input error')).toBeTruthy()
+  render(<WrappedDateInput {...props} />)
+
+  expect(screen.getByText('input error')).toBeInTheDocument()
 })
 
 test('DateInput updates the value on change', async () => {
-  const user = userEvent.setup()
   render(<WrappedDateInput />)
 
+  const user = userEvent.setup()
   const input = screen.getByRole('textbox', { name: 'Field' })
 
-  await user.click(input)
-  await user.keyboard('02/02/2023{Enter}')
+  await user.type(input, '02/02/2023{Enter}')
 
   expect(input).toHaveValue('02/02/2023')
 })
@@ -48,6 +46,7 @@ test('DateInput updates the value on change', async () => {
 test('DateInput sets the placeholder text correctly', () => {
   const props = { placeholderText: 'Test Placeholder' }
   render(<WrappedDateInput {...props} />)
+
   expect(screen.getByPlaceholderText('Test Placeholder')).toBeInTheDocument()
 })
 
@@ -58,11 +57,14 @@ test('DateInput invokes onChange with a Date object', async () => {
   render(<WrappedDateInput {...props} />)
 
   await user.click(screen.getByRole('textbox'))
-  const option = screen.getAllByRole('option', { selected: false, hidden: false } ).at(0)
+
+  const option = screen
+    .getAllByRole('option', { selected: false, hidden: false })
+    .at(0)
   await user.click(option)
 
   expect(onChange).toHaveBeenCalledTimes(1)
-  expect(onChange.mock.calls[0][0] instanceof Date).toBe(true)
+  expect(onChange).toHaveBeenCalledWith(expect.any(Date))
 })
 
 test("DateInput defaults tabbable item to today's date", async () => {
@@ -73,10 +75,7 @@ test("DateInput defaults tabbable item to today's date", async () => {
   const dateInput = screen.getByRole('textbox')
 
   await user.click(dateInput)
-
   const current = screen.getByRole('option', { current: 'date' })
-  const today = moment().format('dddd, MMMM Do, YYYY')
-  const currentDateAriaALabel = screen.getByLabelText(`Choose ${today}`)
-  expect(current).toBeTruthy()
-  expect(currentDateAriaALabel).toBeTruthy()
+
+  expect(current).toHaveProperty('tabIndex', 0)
 })
