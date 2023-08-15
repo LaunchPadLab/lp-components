ac4284690cad750309abe0714ba7790f466be05f
Josias Schneider <josias.ds@gmail.com>
2023-08-15 13:35:44 -0300

    Update Spinner component

--- test/indicators/spinner.test.js
+++ test/indicators/spinner.test.js
@@ -7,7 +7,31 @@ test('Spinner has a defined role', () => {
   expect(screen.getByRole('progressbar')).toBeInTheDocument()
 })
 
+test('Spinner has class "spinner"', () => {
+  render(<Spinner />)
+  const spinner = screen.getByRole('progressbar')
+
+  expect(spinner).toHaveClass('spinner')
+})
+
 test('Spinner can have a custom className', () => {
   render(<Spinner className="custom" />)
-  expect(screen.getByRole('progressbar')).toHaveClass('custom')
+  const spinner = screen.getByRole('progressbar')
+
+  expect(spinner).toHaveClass('spinner', 'custom')
+})
+
+test('Spinner passes props', () => {
+  render(<Spinner name="Bob" />)
+  const spinner = screen.getByRole('progressbar')
+
+  expect(spinner).toHaveAttribute('name', 'Bob')
+})
+
+test('Spinner filters invalid props', () => {
+  render(<Spinner name="Bob" someInvalidProp="I am not a valid DOM prop" />)
+  const spinner = screen.getByRole('progressbar')
+
+  expect(spinner).toHaveAttribute('name', 'Bob')
+  expect(spinner).not.toHaveAttribute('someInvalidProp')
 })
