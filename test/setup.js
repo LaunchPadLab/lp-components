ce8827bfa561b222935274bbb8c3384e11e21de0
Conor Hawes <conor@launchpadlab.com>
2023-08-25 16:28:28 -0400

    Remove enzyme

--- test/setup.js
+++ test/setup.js
@@ -2,12 +2,6 @@
 import 'core-js/stable'
 import 'regenerator-runtime/runtime.js'
 
-import enzyme from 'enzyme'
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
-
-// Configure enzyme
-enzyme.configure({ adapter: new Adapter() })
-
 // Mock browser canvas
 import 'jest-canvas-mock'
 
