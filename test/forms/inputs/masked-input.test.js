cc262709e8d195b4f0a00eb27f6fe6468cc0ed25
Conor Hawes <conor@launchpadlab.com>
2023-09-05 15:48:44 -0400

    Increase masked input coverage

--- test/forms/inputs/masked-input.test.js
+++ test/forms/inputs/masked-input.test.js
@@ -8,34 +8,48 @@ const value = 'value of field'
 const onChange = () => {}
 const input = { name, value, onChange }
 
-test('MaskedInput applies comma-separated number mask', async () => {
-  const user = userEvent.setup()
-  render(
-    <MaskedInput input={input} meta={{}} maskOptions={{ numeral: true }} />
-  )
-  const maskedInput = screen.getByRole('textbox')
-  maskedInput.focus()
-  await user.keyboard('1234')
-  expect(maskedInput.value).toEqual('1,234')
-})
+describe('MaskedInput', () => {
+  test('applies comma-separated number mask', async () => {
+    const user = userEvent.setup()
+    render(
+      <MaskedInput input={input} meta={{}} maskOptions={{ numeral: true }} />
+    )
+    const maskedInput = screen.getByRole('textbox')
+    maskedInput.focus()
+    await user.keyboard('1234')
+    expect(maskedInput.value).toEqual('1,234')
+  })
 
-test('MaskedInput accepts forwarded ref attribute', () => {
-  const ref = React.createRef()
-  render(<MaskedInput input={input} meta={{}} htmlRef={ref} />)
-  expect(screen.getByRole('textbox')).toHaveAttribute('id', ref.current.id)
-})
+  test('accepts forwarded ref attribute', () => {
+    const ref = React.createRef()
+    render(<MaskedInput input={input} meta={{}} htmlRef={ref} />)
+    expect(screen.getByRole('textbox')).toHaveAttribute('id', ref.current.id)
+  })
 
-test('MaskedInput accepts forwarded callback ref', () => {
-  const ref = React.createRef()
-  const callbackRef = (el) => (ref.current = el)
-  render(
-    <MaskedInput input={input} meta={{}} htmlRef={callbackRef} />
-  )
-  expect(screen.getByRole('textbox')).toHaveAttribute('id', ref.current.id)
-})
+  test('accepts forwarded callback ref', () => {
+    const ref = React.createRef()
+    const callbackRef = (el) => (ref.current = el)
+    render(
+      <MaskedInput input={input} meta={{}} htmlRef={callbackRef} />
+    )
+    expect(screen.getByRole('textbox')).toHaveAttribute('id', ref.current.id)
+  })
 
-test('MaskedInput triggers onInit handler', () => {
-  const onInit = jest.fn()
-  render(<MaskedInput input={input} meta={{}} onInit={onInit} />)
-  expect(onInit).toHaveBeenCalledTimes(1)
+  test('triggers onInit handler', () => {
+    const onInit = jest.fn()
+    render(<MaskedInput input={input} meta={{}} onInit={onInit} />)
+    expect(onInit).toHaveBeenCalledTimes(1)
+  })
+
+  test('triggers onInit handler', () => {
+    const onInit = jest.fn()
+    render(<MaskedInput input={input} meta={{}} onInit={onInit} />)
+    expect(onInit).toHaveBeenCalledTimes(1)
+  })
+
+  test('is given an aria-describedby attribute when there is an input error', () => {
+    const props = { input, meta: { touched: true, invalid: true } }
+    render(<MaskedInput {...props} />)
+    expect(screen.getByLabelText('Field').getAttribute('aria-describedby')).toContain(name)
+  })
 })
