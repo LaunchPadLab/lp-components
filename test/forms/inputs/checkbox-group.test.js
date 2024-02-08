bcb472993adbbda9bb8cc55a6831de4d042800a0
Ji Hye Park <ji@launchpadlab.com>
2024-02-08 13:59:13 -0600

    wrap tests under describe

--- test/forms/inputs/checkbox-group.test.js
+++ test/forms/inputs/checkbox-group.test.js
@@ -28,163 +28,164 @@ const WrappedCheckboxGroup = (props) => {
   return <CheckboxGroup {...defaultProps} {...props} />
 }
 
-test('CheckboxGroup adds value to array when unselected option clicked', async () => {
-  const user = userEvent.setup()
-
-  render(<WrappedCheckboxGroup />)
-
-  const checkbox1 = screen.getByRole('checkbox', { name: 'First Option' })
-  const checkbox2 = screen.getByRole('checkbox', { name: 'Second Option' })
-  const checkbox3 = screen.getByRole('checkbox', { name: 'Third Option' })
-
-  await user.click(checkbox2)
-  await user.click(checkbox3)
-
-  expect(checkbox1).not.toBeChecked()
-  expect(checkbox2).toBeChecked()
-  expect(checkbox3).toBeChecked()
-})
-
-test('CheckboxGroup removes value from array when selected option clicked', async () => {
-  const user = userEvent.setup()
-
-  render(<WrappedCheckboxGroup />)
-
-  const checkbox2 = screen.getByRole('checkbox', { name: 'Second Option' })
-  await user.click(checkbox2)
-
-  expect(checkbox2).toBeChecked()
-
-  await user.click(checkbox2)
-
-  expect(checkbox2).not.toBeChecked()
-})
-
-test("CheckboxGroup has a legend with the group's name by default", () => {
-  const props = {
-    input: {
-      name,
-      value: '',
-    },
-    meta: {},
-  }
-  render(<CheckboxGroup {...props} />)
-
-  expect(screen.getByText(formattedName)).toBeInTheDocument()
-})
-
-test("CheckboxGroup has a legend with the group's label (when provided)", () => {
-  const props = {
-    input: {
-      name,
-      value: '',
-    },
-    label: 'Different Name',
-    meta: {},
-  }
-  render(<CheckboxGroup {...props} />)
-
-  expect(screen.getByText('Different Name')).toBeInTheDocument()
-})
-
-test('CheckboxGroup does not pass class to children', () => {
-  const props = {
-    input: {
-      name,
-      value: '',
-    },
-    meta: {},
-    options: ['TOGGLED_OPTION'],
-    className: 'custom-class',
-  }
-  render(<CheckboxGroup {...props} />)
-
-  const checkboxGroup = screen.getByRole('group', { name: formattedName })
-  const checkbox = screen.getByRole('checkbox')
-
-  expect(checkboxGroup).toHaveClass('custom-class')
-  expect(checkbox).not.toHaveClass('custom-class')
-})
-
-test('CheckboxGroup passes down props to children', () => {
-  const props = {
-    input: {
-      name,
-      value: '',
-    },
-    meta: {},
-    options: ['TOGGLED_OPTION'],
-    className: 'custom-group-class',
-    checkboxInputProps: {
-      className: 'custom-input-class',
-    },
-  }
-  render(<CheckboxGroup {...props} />)
-
-  const checkboxGroup = screen.getByRole('group', { name: formattedName })
-  const checkbox = screen.getByRole('checkbox')
-
-  expect(checkboxGroup).toHaveClass('custom-group-class')
-  expect(checkbox).not.toHaveClass('custom-group-class')
-  expect(checkbox).toHaveClass('custom-input-class')
-})
-
-test('CheckboxGroup with dropdown = true adds value to array when unselected option clicked', async () => {
-  const user = userEvent.setup()
-
-  render(<WrappedCheckboxGroup dropdown={true} />)
-
-  const select = screen.getByRole('button')
-  await user.click(select)
-
-  const firstCheckbox = screen.getByLabelText('First Option')
-  await user.click(firstCheckbox)
-
-  const thirdCheckbox = screen.getByLabelText('Third Option')
-  await user.click(thirdCheckbox)
-
-  expect(firstCheckbox).toBeChecked()
-  expect(thirdCheckbox).toBeChecked()
-
-  const selectValueLabel = screen.getByText('3, 1')
-  expect(selectValueLabel).toBeInTheDocument()
-})
-
-test('CheckboxGroup with dropdown = true removes value from array when selected option clicked', async () => {
-  const user = userEvent.setup()
-
-  render(<WrappedCheckboxGroup value={['1']} dropdown={true} />)
-
-  const select = screen.getByRole('button')
-  await user.click(select)
-
-  const firstCheckbox = screen.getByLabelText('First Option')
-  await user.click(firstCheckbox)
-
-  expect(firstCheckbox).not.toBeChecked()
-
-  const selectValueLabel = screen.getByText('None')
-  expect(selectValueLabel).toBeInTheDocument()
-})
-
-test('CheckboxGroup with dropdown = true sets menu no longer active when clicked outside', async () => {
-  const user = userEvent.setup()
-
-  render(<WrappedCheckboxGroup value={['1']} dropdown={true} />)
-
-  const select = screen.getByRole('button')
-  await user.click(select)
-
-  expect(select.nextSibling).toHaveClass('options', 'is-active')
-
-  const fieldset = screen.getAllByRole('group').at(0)
-
-  user.click(fieldset)
-  await waitFor(() => {
-    expect(select.nextSibling).not.toHaveClass('is-active')
-  })
-})
 describe('CheckboxGroup', () => {
+  test('adds value to array when unselected option clicked', async () => {
+    const user = userEvent.setup()
+
+    render(<WrappedCheckboxGroup />)
+
+    const checkbox1 = screen.getByRole('checkbox', { name: 'First Option' })
+    const checkbox2 = screen.getByRole('checkbox', { name: 'Second Option' })
+    const checkbox3 = screen.getByRole('checkbox', { name: 'Third Option' })
+
+    await user.click(checkbox2)
+    await user.click(checkbox3)
+
+    expect(checkbox1).not.toBeChecked()
+    expect(checkbox2).toBeChecked()
+    expect(checkbox3).toBeChecked()
+  })
+
+  test('removes value from array when selected option clicked', async () => {
+    const user = userEvent.setup()
+
+    render(<WrappedCheckboxGroup />)
+
+    const checkbox2 = screen.getByRole('checkbox', { name: 'Second Option' })
+    await user.click(checkbox2)
+
+    expect(checkbox2).toBeChecked()
+
+    await user.click(checkbox2)
+
+    expect(checkbox2).not.toBeChecked()
+  })
+
+  test("has a legend with the group's name by default", () => {
+    const props = {
+      input: {
+        name,
+        value: '',
+      },
+      meta: {},
+    }
+    render(<CheckboxGroup {...props} />)
+
+    expect(screen.getByText(formattedName)).toBeInTheDocument()
+  })
+
+  test("has a legend with the group's label (when provided)", () => {
+    const props = {
+      input: {
+        name,
+        value: '',
+      },
+      label: 'Different Name',
+      meta: {},
+    }
+    render(<CheckboxGroup {...props} />)
+
+    expect(screen.getByText('Different Name')).toBeInTheDocument()
+  })
+
+  test('does not pass class to children', () => {
+    const props = {
+      input: {
+        name,
+        value: '',
+      },
+      meta: {},
+      options: ['TOGGLED_OPTION'],
+      className: 'custom-class',
+    }
+    render(<CheckboxGroup {...props} />)
+
+    const checkboxGroup = screen.getByRole('group', { name: formattedName })
+    const checkbox = screen.getByRole('checkbox')
+
+    expect(checkboxGroup).toHaveClass('custom-class')
+    expect(checkbox).not.toHaveClass('custom-class')
+  })
+
+  test('passes down props to children', () => {
+    const props = {
+      input: {
+        name,
+        value: '',
+      },
+      meta: {},
+      options: ['TOGGLED_OPTION'],
+      className: 'custom-group-class',
+      checkboxInputProps: {
+        className: 'custom-input-class',
+      },
+    }
+    render(<CheckboxGroup {...props} />)
+
+    const checkboxGroup = screen.getByRole('group', { name: formattedName })
+    const checkbox = screen.getByRole('checkbox')
+
+    expect(checkboxGroup).toHaveClass('custom-group-class')
+    expect(checkbox).not.toHaveClass('custom-group-class')
+    expect(checkbox).toHaveClass('custom-input-class')
+  })
+
+  test('with dropdown = true adds value to array when unselected option clicked', async () => {
+    const user = userEvent.setup()
+
+    render(<WrappedCheckboxGroup dropdown={true} />)
+
+    const select = screen.getByRole('button')
+    await user.click(select)
+
+    const firstCheckbox = screen.getByLabelText('First Option')
+    await user.click(firstCheckbox)
+
+    const thirdCheckbox = screen.getByLabelText('Third Option')
+    await user.click(thirdCheckbox)
+
+    expect(firstCheckbox).toBeChecked()
+    expect(thirdCheckbox).toBeChecked()
+
+    const selectValueLabel = screen.getByText('3, 1')
+    expect(selectValueLabel).toBeInTheDocument()
+  })
+
+  test('with dropdown = true removes value from array when selected option clicked', async () => {
+    const user = userEvent.setup()
+
+    render(<WrappedCheckboxGroup value={['1']} dropdown={true} />)
+
+    const select = screen.getByRole('button')
+    await user.click(select)
+
+    const firstCheckbox = screen.getByLabelText('First Option')
+    await user.click(firstCheckbox)
+
+    expect(firstCheckbox).not.toBeChecked()
+
+    const selectValueLabel = screen.getByText('None')
+    expect(selectValueLabel).toBeInTheDocument()
+  })
+
+  test('with dropdown = true sets menu no longer active when clicked outside', async () => {
+    const user = userEvent.setup()
+
+    render(<WrappedCheckboxGroup value={['1']} dropdown={true} />)
+
+    const select = screen.getByRole('button')
+    await user.click(select)
+
+    expect(select.nextSibling).toHaveClass('options', 'is-active')
+
+    const fieldset = screen.getAllByRole('group').at(0)
+
+    user.click(fieldset)
+    await waitFor(() => {
+      expect(select.nextSibling).not.toHaveClass('is-active')
+    })
+  })
+
   test('does not show required indicator when no custom required indicator provided', () => {
     render(<WrappedCheckboxGroup required />)
     expect(screen.getByText(formattedName).textContent).toEqual(formattedName)
--- test/forms/inputs/radio-group.test.js
+++ test/forms/inputs/radio-group.test.js
@@ -6,144 +6,146 @@ import { RadioGroup } from '../../../src/'
 const name = 'testGroup'
 const formattedName = 'Test Group'
 
-test('RadioGroup changes value when buttons are clicked', async () => {
-  const user = userEvent.setup()
-  const onChange = jest.fn()
-  const props = {
-    input: {
-      name,
-      value: '',
-      onChange,
-    },
-    meta: {},
-    options: ['Option 1', 'Option 2'],
-  }
-  render(<RadioGroup {...props} />)
-  const radioOptions = screen.getAllByRole('radio')
-  await user.click(radioOptions.at(0))
-  expect(onChange).toHaveBeenCalledWith('Option 1')
-  await user.click(radioOptions.at(1))
-  expect(onChange).toHaveBeenCalledWith('Option 2')
-})
-
-test("RadioGroup's inputs all have the same name", () => {
-  const props = {
-    input: {
-      name,
-      value: '',
-    },
-    meta: {},
-    options: ['Option 1', 'Option 2'],
-  }
-  render(<RadioGroup {...props} />)
-  const radioOptions = screen.getAllByRole('radio')
-  expect(radioOptions.at(0)).toHaveAttribute('name', name)
-  expect(radioOptions.at(1)).toHaveAttribute('name', name)
-})
-
-test("RadioGroup input has a value that matches the corresponding option's value", () => {
-  const options = ['Option 1', 'Option 2']
-  const props = {
-    input: {
-      name: 'test',
-      value: '',
-    },
-    meta: {},
-    options,
-  }
-  render(<RadioGroup {...props} />)
-  expect(screen.getByRole('radio', { name: options.at(0) })).toHaveAttribute(
-    'value',
-    options.at(0)
-  )
-  expect(screen.getByRole('radio', { name: options.at(1) })).toHaveAttribute(
-    'value',
-    options.at(1)
-  )
-})
-
-test("RadioGroup has a legend with the input's name start-cased by default", () => {
-  const props = {
-    input: {
-      name,
-      value: '',
-    },
-    meta: {},
-    options: ['Option 1', 'Option 2'],
-  }
-  render(<RadioGroup {...props} />)
-  expect(screen.getByRole('group', { name: formattedName })).toBeInTheDocument()
-})
-
-test('RadioGroup has a legend with visually-hidden class when label is `false`', () => {
-  const props = {
-    input: {
-      name,
-      value: '',
-    },
-    meta: {},
-    options: ['Option 1', 'Option 2'],
-    label: false,
-  }
-  render(<RadioGroup {...props} />)
-  const fieldset = screen.queryByRole('group', { name: formattedName })
-  expect(fieldset).toBeInTheDocument()
-  expect(fieldset.firstChild).toHaveClass('visually-hidden')
-})
-
-test("RadioGroup has a legend with the group's label (when provided)", () => {
-  const props = {
-    input: {
-      name,
-      value: '',
-    },
-    meta: {},
-    label: 'Different Name',
-    options: ['Option 1', 'Option 2'],
-  }
-  render(<RadioGroup {...props} />)
-  expect(
-    screen.getByRole('group', { name: 'Different Name' })
-  ).toBeInTheDocument()
-})
-
-test('RadioGroup does not pass down class name', () => {
-  const props = {
-    input: {
-      name: 'test',
-      value: '',
-    },
-    meta: {},
-    options: ['Option 1'],
-    className: 'custom-radio',
-  }
-  render(<RadioGroup {...props} />)
-  expect(screen.getByRole('group', { name: 'Test' })).toHaveClass(
-    'custom-radio'
-  )
-  expect(screen.getByRole('radio')).not.toHaveClass('custom-radio')
-})
-
-test('RadioGroup passes down props to children', () => {
-  const props = {
-    input: {
-      name,
-      value: '',
-    },
-    meta: {},
-    options: ['Option 1', 'Option 2'],
-    className: 'custom-radio-group',
-    'data-test': 'true',
-    radioInputProps: { className: 'custom-radio-input' },
-  }
-  render(<RadioGroup {...props} />)
-  screen.getAllByRole('radio').forEach((el) => {
-    expect(el).toHaveClass('custom-radio-input')
-    expect(el).toHaveAttribute('data-test', 'true')
-  })
-})
-
 describe('RadioGroup', () => {
+  test('changes value when buttons are clicked', async () => {
+    const user = userEvent.setup()
+    const onChange = jest.fn()
+    const props = {
+      input: {
+        name,
+        value: '',
+        onChange,
+      },
+      meta: {},
+      options: ['Option 1', 'Option 2'],
+    }
+    render(<RadioGroup {...props} />)
+    const radioOptions = screen.getAllByRole('radio')
+    await user.click(radioOptions.at(0))
+    expect(onChange).toHaveBeenCalledWith('Option 1')
+    await user.click(radioOptions.at(1))
+    expect(onChange).toHaveBeenCalledWith('Option 2')
+  })
+
+  test('inputs all have the same name', () => {
+    const props = {
+      input: {
+        name,
+        value: '',
+      },
+      meta: {},
+      options: ['Option 1', 'Option 2'],
+    }
+    render(<RadioGroup {...props} />)
+    const radioOptions = screen.getAllByRole('radio')
+    expect(radioOptions.at(0)).toHaveAttribute('name', name)
+    expect(radioOptions.at(1)).toHaveAttribute('name', name)
+  })
+
+  test("input has a value that matches the corresponding option's value", () => {
+    const options = ['Option 1', 'Option 2']
+    const props = {
+      input: {
+        name: 'test',
+        value: '',
+      },
+      meta: {},
+      options,
+    }
+    render(<RadioGroup {...props} />)
+    expect(screen.getByRole('radio', { name: options.at(0) })).toHaveAttribute(
+      'value',
+      options.at(0)
+    )
+    expect(screen.getByRole('radio', { name: options.at(1) })).toHaveAttribute(
+      'value',
+      options.at(1)
+    )
+  })
+
+  test("has a legend with the input's name start-cased by default", () => {
+    const props = {
+      input: {
+        name,
+        value: '',
+      },
+      meta: {},
+      options: ['Option 1', 'Option 2'],
+    }
+    render(<RadioGroup {...props} />)
+    expect(
+      screen.getByRole('group', { name: formattedName })
+    ).toBeInTheDocument()
+  })
+
+  test('has a legend with visually-hidden class when label is `false`', () => {
+    const props = {
+      input: {
+        name,
+        value: '',
+      },
+      meta: {},
+      options: ['Option 1', 'Option 2'],
+      label: false,
+    }
+    render(<RadioGroup {...props} />)
+    const fieldset = screen.queryByRole('group', { name: formattedName })
+    expect(fieldset).toBeInTheDocument()
+    expect(fieldset.firstChild).toHaveClass('visually-hidden')
+  })
+
+  test("has a legend with the group's label (when provided)", () => {
+    const props = {
+      input: {
+        name,
+        value: '',
+      },
+      meta: {},
+      label: 'Different Name',
+      options: ['Option 1', 'Option 2'],
+    }
+    render(<RadioGroup {...props} />)
+    expect(
+      screen.getByRole('group', { name: 'Different Name' })
+    ).toBeInTheDocument()
+  })
+
+  test('does not pass down class name', () => {
+    const props = {
+      input: {
+        name: 'test',
+        value: '',
+      },
+      meta: {},
+      options: ['Option 1'],
+      className: 'custom-radio',
+    }
+    render(<RadioGroup {...props} />)
+    expect(screen.getByRole('group', { name: 'Test' })).toHaveClass(
+      'custom-radio'
+    )
+    expect(screen.getByRole('radio')).not.toHaveClass('custom-radio')
+  })
+
+  test('passes down props to children', () => {
+    const props = {
+      input: {
+        name,
+        value: '',
+      },
+      meta: {},
+      options: ['Option 1', 'Option 2'],
+      className: 'custom-radio-group',
+      'data-test': 'true',
+      radioInputProps: { className: 'custom-radio-input' },
+    }
+    render(<RadioGroup {...props} />)
+    screen.getAllByRole('radio').forEach((el) => {
+      expect(el).toHaveClass('custom-radio-input')
+      expect(el).toHaveAttribute('data-test', 'true')
+    })
+  })
+
   test('does not show required indicator when no custom required indicator provided', () => {
     const props = {
       input: {
